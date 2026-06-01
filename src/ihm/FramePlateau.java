171eba5c4ace7c1a790a28685f14ee098ba9d217
Adrien <a.lequement@hotmail.com>
2026-06-01 14:49:12 +0200

    Test de plateau avec image

--- src/ihm/FramePlateau.java
+++ src/ihm/FramePlateau.java
@@ -1,20 +1,20 @@
 package src.ihm;
 
-import javax.swing.*;
-
 import src.Controleur;
+
+import javax.swing.JFrame;
 
 public class FramePlateau extends JFrame
 {
 
 	public FramePlateau ( Controleur ctrl )
 	{
-		this.setTitle   ( "Dessin");
-		this.setSize    ( 640,660 );
-		this.setLocation(  50, 50 );
+		this.setTitle   ( "Butine!" );
+		this.setSize    ( 640,660   );
+		this.setLocation(  50, 50   );
 
+		this.add( new PanelPlateau(ctrl) );
 
-		// Gestion de la fermeture de la fenêtre
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		this.setVisible(true);
