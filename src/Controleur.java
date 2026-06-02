a48507f467a2901becb2357abf571c79ac3d6a09
Abdellah Achafik <abdoachafik@gmail.com>
2026-06-02 10:39:41 +0200

    Update Controleur

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,12 +1,25 @@
 package src;
 
-import src.ihm.FramePlateau;
+import src.ihm.*;
+import src.metier.*;
 
 public class Controleur
 {
+	private Metier metier;
+	private FramePlateau frame;
+
+	public Controleur()
+	{
+		this.metier = new Metier();
+		this.frame  = new FramePlateau(this);
+	}
+
+	// Méthodes du jeu en appelant Metier
+	/* public boolean deplacer... */
+	/* public Fleur   getFleur... */
+
 	public static void main(String[] args)
 	{
-		Controleur ctrl = new Controleur();
-		new FramePlateau(ctrl);
+		new Controleur();
 	}
 }
