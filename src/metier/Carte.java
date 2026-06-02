d8af924c565469bc408371fc48688415adf10541
Adrien <a.lequement@hotmail.com>
2026-06-02 08:39:15 +0200

    ajout de la class Carte.java

---
+++ src/metier/Carte.java
@@ -0,0 +1,21 @@
+package src.metier;
+
+public class Carte
+{
+	private char forme;
+	private boolean couleur;
+
+	public Carte () {}
+	
+	public Carte (char f, boolean c)
+	{
+		this.forme   = f;
+		this.couleur = c;
+	}
+
+	public void setForme   (char f   ) {this.forme = f;  }
+	public void setCouleur (boolean c) {this.couleur = c;}
+
+	public char    getForme   () {return this.forme;  }
+	public boolean getCouleur () {return this.couleur;}
+}
