a175f3f3345fc5f1e6b9e14ae6e1f1515dd8ebf0
Abdellah Achafik <abdoachafik@gmail.com>
2026-06-02 10:42:50 +0200

    Init FramePlateau

--- src/ihm/FramePlateau.java
+++ src/ihm/FramePlateau.java
@@ -1,75 +1,26 @@
 package src.ihm;
 
-import java.awt.*;
 import javax.swing.*;
 import src.Controleur;
-import src.metier.Grille;
 
 public class FramePlateau extends JFrame
 {
-	private PanelGrille panelGrille;
-	private JTextField fieldLargeur;
-	private JTextField fieldHauteur;
-	private JTextField fieldTailleCase;
+	private Controleur   ctrl;
+	private PanelPlateau panelPlateau;
 
 	public FramePlateau(Controleur ctrl)
 	{
-		this.setTitle("Grille");
+		this.ctrl = ctrl;
+
+        this.setTitle("Plateau");
 		this.setSize(750, 750);
-		this.setLocationRelativeTo(null);
+
+
+		this.panelPlateau = new PanelPlateau(ctrl);
+		this.add(this.panelPlateau);
+		
+
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JPanel panelHaut = new JPanel();
-		panelHaut.add(new JLabel("Largeur:"));
-		fieldLargeur = new JTextField("10", 5);
-		panelHaut.add(fieldLargeur);
-
-		panelHaut.add(new JLabel("Hauteur:"));
-		fieldHauteur = new JTextField("10", 5);
-		panelHaut.add(fieldHauteur);
-
-		panelHaut.add(new JLabel("Taille case:"));
-		fieldTailleCase = new JTextField("50", 5);
-		panelHaut.add(fieldTailleCase);
-
-		JButton btnCreer = new JButton("Créer");
-		btnCreer.addActionListener(e -> creerGrille());
-		panelHaut.add(btnCreer);
-
-		panelGrille = new PanelGrille(null);
-
-		this.getContentPane().setLayout(new BorderLayout());
-		this.getContentPane().add(panelHaut, BorderLayout.NORTH);
-		this.getContentPane().add(panelGrille, BorderLayout.CENTER);
-
 		this.setVisible(true);
-	}
-
-	private void creerGrille()
-	{
-		try
-		{
-			int largeur = Integer.parseInt(fieldLargeur.getText());
-			int hauteur = Integer.parseInt(fieldHauteur.getText());
-			int taille = Integer.parseInt(fieldTailleCase.getText());
-
-
-
-			if (largeur > 0 && hauteur > 0 && taille > 0 &&
-				largeur <= 10 && hauteur <= 10 && taille <= 100)
-			{
-				Grille grille = new Grille(largeur, hauteur);
-				panelGrille.setGrille(grille);
-				panelGrille.setTailleCase(taille);
-			}
-			else
-			{
-				JOptionPane.showMessageDialog(this, "Merci de choisir une hauteur et largeur entre 1 et 10");
-			}
-		}
-		catch (NumberFormatException e)
-		{
-			JOptionPane.showMessageDialog(this, "Erreur");
-		}
 	}
 }
