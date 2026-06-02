03cd086c09cdcd617e7777b4c5226ec7d3a9503b
Abdellah Achafik <abdoachafik@gmail.com>
2026-06-02 15:39:42 +0200

    ajout methode pour image accueil

--- src/Controleur.java
+++ src/Controleur.java
@@ -5,19 +5,25 @@ import src.metier.*;
 
 public class Controleur
 {
-	private Metier metier;
+	private Metier       metier;
 	private FramePlateau frame;
+
+	private String ficImgAccueil;
 
 	public Controleur()
 	{
+		this.ficImgAccueil = "../images/background.jpg";
+
 		this.metier = new Metier();
-		this.frame  = new FramePlateau(this);
+		this.frame  = new FramePlateau( this );
 	}
 
 	public Grille creerGrille(int largeur, int hauteur, int taille)
 	{
 		return new Grille( largeur, hauteur, taille );
 	}
+
+	public String getImageAcceuil() { return this.ficImgAccueil; }
 
 	// Méthodes du jeu en appelant Metier
 	/* public boolean deplacer... */
