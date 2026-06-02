5da10e4da43b0604af66d1ea3b6fb485ce16d46a
Abdellah Achafik <abdoachafik@gmail.com>
2026-06-02 10:44:23 +0200

    Init fichiers structure Metier

---
+++ src/metier/Plaine.java
@@ -0,0 +1,22 @@
+package src.metier;
+
+import java.awt.Color;
+
+public class Plaine
+{
+	private Color couleur;
+
+	public Plaine()
+	{
+	}
+
+	public Plaine(Color c)
+	{
+		this.couleur = c;
+	}
+
+	public Color getCouleur()
+	{
+		return this.couleur;
+	}
+}
