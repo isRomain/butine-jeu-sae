b9735123c96144707595aa7b255d875a2801bca2
Romain <romain.fra@icloud.com>
2026-06-03 09:40:03 +0200

    Ajout de la sélection des zones dans PanelConstruction

--- src/ihm/PanelContruction.java
+++ src/ihm/PanelContruction.java
@@ -13,37 +13,43 @@ public class PanelContruction extends JPanel implements ItemListener
 {
 	private FramePlateau prnt;
 
-	private JComboBox listtype, listCouleur, listFormes;
+	private JComboBox listType, listCouleur, listFormes, listPlaines;
+
 	private String[]  comboStringType     = {"fleur", "zone"};
 	private String[]  comboStringCouleurs = {"neutre", "rouge", "vert", "bleu", "marron"};
 	private String[]  comboStringFormes   = {"carre", "rond", "triangle", "croix"};
+	private String[]  comboStringPlaines   = {"Capucine", "Pivoine", "Chèvrefeuille", "Primevère", "Menthe", "Myosotis", "Lavande", "Lilas", "Glycine", "Cerisier", "Sauge", "Tilleul"};
 
-	private JButton   bouton;
+	private JButton   btnIcon;
 
 	public PanelContruction (FramePlateau prnt)
 	{
 		this.prnt = prnt;
 		this.setLayout( new GridLayout(1, 4) );
 
-		this.listtype    = new JComboBox<String>(comboStringType);
+		this.listType    = new JComboBox<String>(comboStringType);
 		this.listCouleur = new JComboBox<String>(comboStringCouleurs);
 		this.listFormes  = new JComboBox<String>(comboStringFormes);
-		this.bouton      = new JButton();
 
-		this.listtype.addItemListener(this);
+		this.listPlaines = new JComboBox<String>(comboStringPlaines);
+
+		this.btnIcon      = new JButton();
+
+		this.listType   .addItemListener(this);
 		this.listCouleur.addItemListener(this);
-		this.listFormes.addItemListener(this);
+		this.listFormes .addItemListener(this);
 
-		this.add( listtype   );
-		this.add( listCouleur);
-		this.add( listFormes );
+		this.add( this.listType    );
+		this.add( this.listCouleur );
+		this.add( this.listFormes  );
+		this.add( this.listPlaines );
 
-		this.add( bouton );
+		this.add( this.btnIcon );
 	}
 
 	public void itemStateChanged(ItemEvent e)
 	{
 		System.out.println(this.listCouleur.getSelectedItem());
-		this.bouton.setIcon( new ImageIcon("../images/pollen_" + this.listFormes.getSelectedItem() + "_" + this.listCouleur.getSelectedItem() + ".jpg") );
+		this.btnIcon.setIcon( new ImageIcon("../images/pollen_" + this.listFormes.getSelectedItem() + "_" + this.listCouleur.getSelectedItem() + ".jpg") );
 	}
 }
