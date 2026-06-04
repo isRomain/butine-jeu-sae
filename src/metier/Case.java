cc6ed822731358cf394c11289f4b5ebb79de9c53
Adrien <a.lequement@hotmail.com>
2026-06-04 08:45:02 +0200

    modiffiaction de la class Case et supretion de class inutile

--- src/metier/Case.java
+++ src/metier/Case.java
@@ -1,21 +1,37 @@
 package src.metier;
 
 import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 public class Case
 {
-	private Fleur  fleur;
-	private Color plaine;
+	private Color  plaine;
+	private String fleur;
+	private String depart;
 
 	public Case ()
 	{
 		this.fleur  = null;
 		this.plaine = new Color(255, 255, 255);
+		this.depart = null;
 	}
 
-	public void setFleur  (Fleur fleur ) {this.fleur = fleur;  }
-	public void setPlaine (Color plaine) {this.plaine = plaine;}
+	public void setPlaine (Color plaine ) {this.plaine = plaine;}
+	public void setFleur  (String fleur ) {this.fleur  = fleur; }
+	public void setDepart (String depart) {this.depart = depart;}
 
-	public Fleur  getFleur () {return this.fleur; }
-	public Color getPlaine () {return this.plaine;}
+	public String getFleur  () {return this.fleur; }
+	public Color  getPlaine () {return this.plaine;}
+	public String getDepart () {return this.depart;}
+
+	public Image getImageFleur ()
+	{
+		return Toolkit.getDefaultToolkit().getImage("../images/pollens/pollen_" + this.fleur + ".png");
+	}
+
+	public Image getImageDepart ()
+	{
+		return Toolkit.getDefaultToolkit().getImage("../images/pollens/depart_" + this.depart + ".png");
+	}
 }
