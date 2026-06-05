c621303a60f701eea00ae7385b15cb2e2044cd36
Max-Malo <maxmalo.basley@gmail.com>
2026-06-05 08:35:46 +0200

    Couleurs plus visibles

--- src/ihm/PanelChoixRegion.java
+++ src/ihm/PanelChoixRegion.java
@@ -21,15 +21,15 @@ public class PanelChoixRegion extends JPanel implements ActionListener
 		new Color(255, 214, 165, 200), // capucine
 		new Color(255, 179, 186, 200), // pivoine
 		new Color(255, 203, 186, 200), // chèvrefeuille
-		new Color(255, 255, 186, 200), // primevère
+		new Color(255, 235, 206, 200), // primevère
 		new Color(204, 236, 197, 200), // menthe
 		new Color(186, 225, 255, 200), // myosotis
 		new Color(196, 217, 255, 200), // lavande
 		new Color(215, 196, 255, 200), // lilas
-		new Color(243, 198, 240, 200), // glycine
-		new Color(255, 198, 224, 200), // cerisier
+		new Color(223, 178, 240, 200), // glycine
+		new Color(255, 248, 204, 200), // cerisier
 		new Color(186, 240, 230, 200), // sauge
-		new Color(225, 243, 198, 200), // tilleul
+		new Color(225, 203, 158, 200), // tilleul
 	};
 
 
