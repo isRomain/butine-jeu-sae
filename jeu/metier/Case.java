1e51d9e41582335241c81d55d033a849e19215c8
Romain <romain.fra@icloud.com>
2026-06-08 08:53:56 +0200

    Ajout de la partie métier

---
+++ jeu/metier/Case.java
@@ -0,0 +1,154 @@
+package conception.metier;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class Case
+{
+	private int    x, y;
+	private Color  plaine;
+	private String fleur;
+	private String depart;
+
+	private Case[] connections;
+
+	public Case (int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+
+		this.plaine = new Color(255, 255, 255, 200);
+		this.fleur  = "vide";
+		this.depart = "vide";
+
+		this.connections = new Case[8];
+	}
+
+	public void setPlaine ( Color plaine  ) { this.plaine = plaine; }
+	public void setFleur  ( String fleur  ) { this.fleur  = fleur ; }
+	public void setDepart ( String depart ) { this.depart = depart; }
+
+	public int    getX      () { return this.x;}
+	public int    getY      () { return this.y;}
+	public Color  getPlaine () { return this.plaine; }
+	public String getFleur  () { return this.fleur;  }
+	public String getDepart () { return this.depart; }
+
+	public Case   getConnection (int nb) {return this.connections[nb];}
+
+	public Image getImageFleur ()
+	{
+		return Toolkit.getDefaultToolkit().getImage("../images/pollens/pollen_" + this.fleur + ".png");
+	}
+
+	public Image getImageDepart ()
+	{
+		return Toolkit.getDefaultToolkit().getImage("../images/contours/contour_case_" + this.depart + ".png");
+	}
+
+	public void trouverConnections (Grille grille)
+	{
+		for (int cpt = 0; cpt < 8; cpt++)
+				this.connections[cpt] = null;
+
+		if (this.fleur.equals("vide"))
+			return;
+
+		// Verifier les cases a gauche
+		for (int check = this.x - 1; check > - 1; check--)
+		{
+			if (!grille.getCase(check, this.y).getFleur().equals("vide"))
+			{
+				this.connections[0] = grille.getCase(check, this.y);
+				break;
+			}
+		}
+
+		// Verifier les cases en haut
+		for (int check = this.y - 1; check > - 1; check--)
+		{
+			if (!grille.getCase(this.x, check).getFleur().equals("vide"))
+			{
+				this.connections[2] = grille.getCase(this.x, check);
+				break;
+			}
+		}
+
+		// Verifier les cases a droite
+		for (int check = this.x + 1; check < grille.getLargeur(); check++)
+		{
+			if (!grille.getCase(check, this.y).getFleur().equals("vide"))
+			{
+				this.connections[4] = grille.getCase(check, this.y);
+				break;
+			}
+		}
+
+		// Verifier les cases en bas
+		for (int check = this.y + 1; check < grille.getHauteur(); check++)
+		{
+			if (!grille.getCase(this.x, check).getFleur().equals("vide"))
+			{
+				this.connections[6] = grille.getCase(this.x, check);
+				break;
+			}
+		}
+
+		// Verifier les cases en haut a gauche
+		int tempX = this.x - 1;
+		int tempY = this.y - 1;
+		while (grille.getCase(tempX, tempY) != null)
+		{
+			if (!grille.getCase(tempX, tempY).getFleur().equals("vide"))
+			{
+				this.connections[1] = grille.getCase(tempX, tempY);
+				break;
+			}
+			tempX -= 1;
+			tempY -= 1;
+		}
+
+		// Verifier les cases en haut a droite
+		tempX = this.x + 1;
+		tempY = this.y - 1;
+		while (grille.getCase(tempX, tempY) != null)
+		{
+			if (!grille.getCase(tempX, tempY).getFleur().equals("vide"))
+			{
+				this.connections[3] = grille.getCase(tempX, tempY);
+				break;
+			}
+			tempX += 1;
+			tempY -= 1;
+		}
+
+		// Verifier les cases en bas a droite
+		tempX = this.x + 1;
+		tempY = this.y + 1;
+		while (grille.getCase(tempX, tempY) != null)
+		{
+			if (!grille.getCase(tempX, tempY).getFleur().equals("vide"))
+			{
+				this.connections[5] = grille.getCase(tempX, tempY);
+				break;
+			}
+			tempX += 1;
+			tempY += 1;
+		}
+
+		// Verifier les cases en bas a gauche
+		tempX = this.x - 1;
+		tempY = this.y + 1;
+		while (grille.getCase(tempX, tempY) != null)
+		{
+			if (!grille.getCase(tempX, tempY).getFleur().equals("vide"))
+			{
+				this.connections[7] = grille.getCase(tempX, tempY);
+				break;
+			}
+			tempX -= 1;
+			tempY += 1;
+		}
+	}
+}
---
+++ jeu/metier/Grille.java
@@ -0,0 +1,107 @@
+package conception.metier;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class Grille
+{
+	private int largeur;
+	private int hauteur;
+	private int tailleCase;
+	private Case[][] cases;
+
+	public Grille(int largeur, int hauteur, int tailleCase)
+	{
+		this.largeur    = largeur;
+		this.hauteur    = hauteur;
+		this.tailleCase = tailleCase;
+		this.cases      = new Case[largeur][hauteur];
+
+		for (int x = 0; x < largeur; x++)
+			for (int y = 0; y < hauteur; y++)
+			{
+				this.cases[x][y] = new Case(x, y);
+			}
+	}
+
+	public int getLargeur   () { return largeur;         }
+	public int getHauteur   () { return hauteur;         }
+	public int getTailleCase() { return this.tailleCase; }
+
+	public Case getCase(int x, int y)
+	{
+		if (x >= 0 && x < largeur && y >= 0 && y < hauteur) return cases[x][y];
+		return null;
+	}
+
+	public void setCase(int x, int y, Case vCase)
+	{
+		if (x >= 0 && x < largeur && y >= 0 && y < hauteur) cases[x][y] = vCase;
+	}
+
+	public boolean regionsConnexes()
+	{
+		boolean[][]      visite       = new boolean[largeur][hauteur];
+		ArrayList<Color> couleursVues = new ArrayList<Color>();
+
+		for (int x = 0; x < largeur; x++)
+		{
+			for (int y = 0; y < hauteur; y++)
+			{
+				Color couleur = getCase(x, y).getPlaine();
+
+				if (!visite[x][y])
+				{
+					if (couleursVues.contains(couleur)) return false;
+
+					couleursVues.add(couleur);
+					remplir(x, y, couleur, visite);
+				}
+			}
+		}
+
+		return true;
+	}
+
+	private void remplir(int x, int y, Color couleur, boolean[][] visite)
+	{
+		if (x < 0 || x >= largeur || y < 0 || y >= hauteur)
+			return;
+
+		if (visite[x][y] || !getCase(x, y).getPlaine().equals(couleur))
+			return;
+
+		visite[x][y] = true;
+
+		remplir(x + 1, y, couleur, visite);
+		remplir(x - 1, y, couleur, visite);
+		remplir(x, y + 1, couleur, visite);
+		remplir(x, y - 1, couleur, visite);
+	}
+
+	public boolean departUnique ()
+	{
+		String departs = "";
+		for (int x = 0; x < this.largeur; x++)
+			for (int y = 0; y < this.hauteur; y++)
+				if (!this.cases[x][y].getDepart().equals("vide"))
+				{
+					if (departs.contains(this.cases[x][y].getDepart()))
+						return false;
+					
+					departs += this.cases[x][y].getDepart();
+				}
+
+		if (departs.equals(""))
+			return false;
+		
+		return true;
+	}
+
+	public void trouverConnections()
+	{
+		for (int x = 0; x < largeur; x++)
+			for (int y = 0; y < hauteur; y++)
+				this.cases[x][y].trouverConnections(this);
+	}
+}
