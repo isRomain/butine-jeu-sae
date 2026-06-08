f824a64523f692666dc9861aa8e2f6d1fa70330e
Abdellah Achafik <abdoachafik@gmail.com>
2026-06-08 14:27:18 +0200

    Ajout de commentaires

--- conception/ihm/PanelGrille.java
+++ conception/ihm/PanelGrille.java
@@ -53,131 +53,6 @@ public class PanelGrille extends JPanel
 
 	public Grille getGrille() { return this.grille; }
 
-	private void actionCase(int pixelX, int pixelY)
-	{
-		int taille = grille.getTailleCase();
-		int x = (pixelX - decalX) / taille;
-		int y = (pixelY - decalY) / taille;
-
-		if (pixelX >= decalX && pixelY >= decalY &&
-			x >= 0 && x < grille.getLargeur() &&
-			y >= 0 && y < grille.getHauteur())
-		{ 
-			if (this.couleurPlaine != null && !this.estPanelExport)
-			{
-				grille.getCase(x, y).setPlaine(couleurPlaine);
-				this.repaint();
-			}
-			if (this.formeFleur != null && this.modeFleurs)
-			{
-				grille.getCase(x, y).setFleur(this.formeFleur);
-				grille.trouverConnections();
-				this.repaint();
-			}
-			if (this.couleurDepart != null && this.modeFleurs)
-			{
-				grille.getCase(x, y).setDepart(this.couleurDepart);
-				this.repaint();
-			}
-		}
-	}
-
-	public void activerRegions()
-	{
-		this.couleurPlaine  = new Color(255, 214, 165, 200);
-		this.modeFleurs     = false;
-		this.estPanelExport = false;
-	}
-	public void effacerTraits()
-	{
-		 this.afficherTraits = false;
-	}
-
-	public void afficherTraits()
-	{
-		 this.afficherTraits = true;
-	}
-
-	public void activerFleurs()
-	{
-		 this.afficherFleurs = true;
-		 this.modeFleurs     = true;
-	}
-
-	public void desactiverFleurs()
-	{
-		 this.afficherFleurs = false;
-	}
-
-	public void estPanelExport()
-	{
-		 this.modeFleurs     = false;
-		 this.estPanelExport = true;
-	}
-
-	protected void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-
-		Image img = Toolkit.getDefaultToolkit().getImage("../images/icones/fond_pre.jpg");
-		g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
-
-
-		if (grille == null)
-			return;
-
-		int largeur = grille.getLargeur();
-		int hauteur = grille.getHauteur();
-
-		int largeurGrille = largeur * grille.getTailleCase();
-		int hauteurGrille = hauteur * grille.getTailleCase();
-
-		decalX = (this.getWidth() - largeurGrille) / 2;
-		decalY = (this.getHeight() - hauteurGrille) / 2;
-
-		for (int y = 0; y < hauteur; y++)
-		{
-			for (int x = 0; x < largeur; x++)
-			{
-				int posX = decalX + x * grille.getTailleCase();
-				int posY = decalY + y * grille.getTailleCase();
-
-				Color couleurCase = grille.getCase(x, y).getPlaine();
-				g.setColor(couleurCase);
-				g.fillRect(posX, posY, grille.getTailleCase(), grille.getTailleCase());
-
-				if( this.afficherTraits )
-				{
-					 g.setColor(Color.BLACK);
-				     g.drawRect(posX, posY, grille.getTailleCase(), grille.getTailleCase());
-				}
-
-				if (grille.getCase(x, y).getDepart() != null)
-				{
-					g.drawImage(grille.getCase(x, y).getImageDepart(), posX, posY, grille.getTailleCase(), grille.getTailleCase(), this);
-				}
-
-				if (grille.getCase(x, y).getFleur() != null && this.afficherFleurs)
-				{
-					g.drawImage(grille.getCase(x, y).getImageFleur(), posX, posY, grille.getTailleCase(), grille.getTailleCase(), this);
-				}
-
-				for (int cpt = 0; cpt < 8; cpt++)
-				{
-					if( this.afficherFleurs )
-					{
-						 Case connection = grille.getCase(x, y).getConnection(cpt);
-						 if (connection != null)
-						 {
-						 	g.setColor(Color.BLACK);
-						 	g.drawLine(posX + grille.getTailleCase()/2, posY + grille.getTailleCase()/2, (decalX + connection.getX() * grille.getTailleCase()) + grille.getTailleCase()/2, (decalY + connection.getY() * grille.getTailleCase()) + grille.getTailleCase()/2);
-						 }
-					}
-				}
-			}
-		}
-	}
-
 	public void setGrille(Grille grille)
 	{
 		this.grille = grille;
@@ -198,7 +73,78 @@ public class PanelGrille extends JPanel
 	{
 		this.couleurDepart = couleur;
 	}
-	
+
+	/* Cette methode est appelée apres un clique sur la souris,   */
+	/* permettant d'affecter une fleur, plaine(couleur) et depart */
+	/* en fonction de la position de la souris (pixelX et pixelY) */
+	private void actionCase(int pixelX, int pixelY)
+	{
+		int taille = grille.getTailleCase();
+		int x = (pixelX - decalX) / taille;
+		int y = (pixelY - decalY) / taille;
+
+		if (pixelX >= decalX && pixelY >= decalY &&
+			x >= 0 && x < grille.getLargeur() &&
+			y >= 0 && y < grille.getHauteur())
+		{ 
+			if (this.couleurPlaine != null && !this.estPanelExport)
+			{
+				grille.getCase(x, y).setPlaine(couleurPlaine); // Affecter la couleur a la case
+				this.repaint();
+			}
+			if (this.formeFleur != null && this.modeFleurs)
+			{
+				grille.getCase(x, y).setFleur(this.formeFleur); // Affecter la fleur a la case
+				grille.trouverConnections();
+				this.repaint();
+			}
+			if (this.couleurDepart != null && this.modeFleurs)
+			{
+				grille.getCase(x, y).setDepart(this.couleurDepart); // Affecter le depart a la case
+				this.repaint();
+			}
+		}
+	}
+
+	/* Methode pour activer les methodes necessaire en panelChoixRegion */
+	public void activerRegions()
+	{
+		this.couleurPlaine  = new Color(255, 214, 165, 200);
+		this.modeFleurs     = false;
+		this.estPanelExport = false;
+	}
+
+	/* Methode pour desactiver les traits de liasons entre fleurs */
+	public void effacerTraits()
+	{
+		 this.afficherTraits = false;
+	}
+
+	/* Methode pour activer les traits de liason */
+	public void afficherTraits()
+	{
+		 this.afficherTraits = true;
+	}
+
+	public void activerFleurs()
+	{
+		 this.afficherFleurs = true;
+		 this.modeFleurs     = true;
+	}
+
+	public void desactiverFleurs()
+	{
+		 this.afficherFleurs = false;
+	}
+
+	/* Methode appelée quand on est sur panelExport */
+	public void estPanelExport()
+	{
+		 this.modeFleurs     = false;
+		 this.estPanelExport = true;
+	}
+
+	/*Vérifie que chaque couleur de plaine forme une seule région connexe */
 	public boolean verifierRegions()
 	{
 		return this.grille.regionsConnexes();
@@ -207,5 +153,74 @@ public class PanelGrille extends JPanel
 	public boolean departUnique()
 	{
 		return this.grille.departUnique();
+	}
+
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+
+		// Image de fond du plateau
+		Image img = Toolkit.getDefaultToolkit().getImage("../images/icones/fond_pre.jpg");
+		g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+
+
+		if (grille == null)
+			return;
+
+		int largeur = grille.getLargeur();
+		int hauteur = grille.getHauteur();
+
+		int largeurGrille = largeur * grille.getTailleCase();
+		int hauteurGrille = hauteur * grille.getTailleCase();
+
+		decalX = (this.getWidth() - largeurGrille) / 2;
+		decalY = (this.getHeight() - hauteurGrille) / 2;
+
+		// Boucle pour creer 
+		for (int y = 0; y < hauteur; y++)
+		{
+			for (int x = 0; x < largeur; x++)
+			{
+				int posX = decalX + x * grille.getTailleCase();
+				int posY = decalY + y * grille.getTailleCase();
+
+				Color couleurCase = grille.getCase(x, y).getPlaine();
+				g.setColor(couleurCase);
+				g.fillRect(posX, posY, grille.getTailleCase(), grille.getTailleCase());
+
+				// Dessiner les cases du plateau
+				if( this.afficherTraits )
+				{
+					 g.setColor(Color.BLACK);
+				     g.drawRect(posX, posY, grille.getTailleCase(), grille.getTailleCase());
+				}
+
+				// Dessiner les images de point de depart
+				if (grille.getCase(x, y).getDepart() != null)
+				{
+					g.drawImage(grille.getCase(x, y).getImageDepart(), posX, posY, grille.getTailleCase(), grille.getTailleCase(), this);
+				}
+
+				// Dessiner les images des fleurs
+				if (grille.getCase(x, y).getFleur() != null && this.afficherFleurs)
+				{
+					g.drawImage(grille.getCase(x, y).getImageFleur(), posX, posY, grille.getTailleCase(), grille.getTailleCase(), this);
+				}
+
+				// Dessiner les traits de laisons entre les fleurs
+				for (int cpt = 0; cpt < 8; cpt++)
+				{
+					if( this.afficherFleurs )
+					{
+						 Case connection = grille.getCase(x, y).getConnection(cpt);
+						 if (connection != null)
+						 {
+						 	g.setColor(Color.BLACK);
+						 	g.drawLine(posX + grille.getTailleCase()/2, posY + grille.getTailleCase()/2, (decalX + connection.getX() * grille.getTailleCase()) + grille.getTailleCase()/2, (decalY + connection.getY() * grille.getTailleCase()) + grille.getTailleCase()/2);
+						 }
+					}
+				}
+			}
+		}
 	}
 }
